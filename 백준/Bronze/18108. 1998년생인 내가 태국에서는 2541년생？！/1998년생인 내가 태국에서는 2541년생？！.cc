de1ae4e32757dd6ac0252679d5f37f69751b43a6
DongHwan Kim <gidskql6671@gmail.com>
2022-06-14 20:22:34 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  int year;
+  cin >> year;
+  cout << year - 543 << endl;
+
+  return 0;
+}
