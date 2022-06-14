28a9559de497a3313a53ad9f9dd09d6e5927eb06
DongHwan Kim <gidskql6671@gmail.com>
2022-06-14 20:28:02 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  if (a == b && b == c) {
+    cout << 10000 + a * 1000 << endl;
+  }
+  else if (a == b || b == c || a == c) {
+    if (a == b || a == c) {
+      cout << 1000 + a * 100 << endl;
+    }
+    else {
+      cout << 1000 + b * 100 << endl;
+    }
+  }
+  else {
+    cout << max(max(a, b), c) * 100 << endl;
+  }
+
+  return 0;
+}
