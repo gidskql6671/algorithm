5abb2be529ed209f41f4e6b0bc4b5b1c950f93e3
DongHwan Kim (Doritos) <gidskql6671@gmail.com>
2023-04-27 09:01:20 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+#define ERROR_STATE 7
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  ll a, b, c;
+  
+  cin >> a >> b >> c;
+  
+  cout << a + b + c << endl;
+
+
+  return 0;
+}
