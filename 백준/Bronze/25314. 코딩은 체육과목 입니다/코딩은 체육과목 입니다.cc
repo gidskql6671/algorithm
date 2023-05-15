8f3a0fd030d798255d2f594718049a8e5a3d54e3
DongHwan Kim (Doritos) <gidskql6671@gmail.com>
2023-05-15 21:40:05 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  int n;
+
+  cin >> n;
+
+  for(int i = 0; i < n / 4; i++) {
+    cout << "long ";
+  }
+
+  cout << "int\n";
+
+  return 0;
+}
