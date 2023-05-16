fe27bee914c05b3532d3bb290f57c4d7574a855b
DongHwan Kim (Doritos) <gidskql6671@gmail.com>
2023-05-16 22:43:02 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  string str;
+  int n;
+
+  cin >> str >> n;
+
+  cout << str[n - 1] << endl;
+
+  return 0;
+}
