f7397272dd142bc05ec584d33ac633f6c8af5da3
DongHwan Kim (Doritos) <gidskql6671@gmail.com>
2023-05-18 08:59:15 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  int n;
+  cin >> n;
+
+  cout << (int)pow(2, n) << endl;
+
+  return 0;
+}
