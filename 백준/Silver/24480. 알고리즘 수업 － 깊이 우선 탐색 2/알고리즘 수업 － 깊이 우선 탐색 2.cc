4ee38f926a84d67765d320214254375ec986382a
DongHwan Kim (Doritos) <gidskql6671@gmail.com>
2023-05-24 16:02:16 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 92 ms, Memory: 10928 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int n, m, r, cnt = 1;
+int visited[100001];
+vector<vector<int>> graph(100001);
+
+void dfs(int cur) {
+  visited[cur] = cnt++;
+
+  for(int i = 0; i < graph[cur].size(); i++) {
+    int next = graph[cur][i];
+
+    if (!visited[next]) {
+       dfs(next);
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  cin >> n >> m >> r;
+  for(int i = 0; i < m; i++) {
+    int tail, head;
+    cin >> tail >> head;
+
+    graph[tail].push_back(head);
+    graph[head].push_back(tail);
+  }
+
+  for(int i = 1; i <= n; i++) {
+    sort(graph[i].begin(), graph[i].end(), greater<>());
+  }
+
+  dfs(r);
+
+  for(int i = 1; i <= n; i++) {
+    cout << visited[i] << "\n";
+  }
+
+  return 0;
+}
