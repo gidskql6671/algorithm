19d2c3b62a25a0a12eaeb90d0c7aac678f1d4cbf
DongHwan Kim (Doritos) <gidskql6671@gmail.com>
2023-05-30 09:37:53 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  ll n;
+  cin >> n;
+
+  cout << n * (n - 1) / 2 << "\n2\n";
+
+  return 0;
+}
