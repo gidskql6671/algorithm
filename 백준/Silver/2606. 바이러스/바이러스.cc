25d4f6d853398611905a2f2f90cc5f6c675b829f
DongHwan Kim <gidskql6671@gmail.com>
2023-11-01 15:16:11 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,20 +1,30 @@
 #include <bits/stdc++.h>
 #define INF 987654321
 typedef long long ll;
-
 using namespace std;
 
-int n, m;
-bool graph[101][101];
-bool visited[101];
 
-int dfs(int cur) {
-  int count = 1;
-  visited[cur] = true;
+int n;
+vector<int> graph[101];
 
-  for(int i = 1; i <= n; i++) {
-    if (graph[cur][i] && !visited[i]) {
-      count += dfs(i);
+int bfs(int start) {
+  bool visited[101] = { false };
+  queue<int> q;
+
+  q.push(start);
+  visited[start] = true;
+
+  int count = 0;
+  while(not q.empty()) {
+    int cur = q.front();
+    q.pop();
+    count++;
+
+    for(int next : graph[cur]) {
+      if (not visited[next]) {
+        q.push(next);
+        visited[next] = true;
+      }
     }
   }
 
@@ -27,17 +37,17 @@ int main() {
   cout.tie(NULL);
 
 
+  int m;
   cin >> n >> m;
-
   for (int i = 0; i < m; i++) {
-    int from, to;
-    cin >> from >> to;
+    int a, b;
+    cin >> a >> b;
 
-    graph[from][to] = true;
-    graph[to][from] = true;
+    graph[a].push_back(b);
+    graph[b].push_back(a);
   }
 
-  cout << dfs(1) - 1 << endl;
+  cout << bfs(1) - 1 << endl;
 
   return 0;
 }
