f3a69c3185860a78989dbb46e235fa9a13f8e5a1
DongHwan Kim <gidskql6671@gmail.com>
2023-11-17 20:44:11 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 #define INF 987654321
 typedef long long ll;
-
 using namespace std;
 
 
-int arr[100001], acuSum[100001];
+int n, s;
+int arr[100001];
 
 int main() {
   ios::sync_with_stdio(false);
@@ -13,34 +13,26 @@ int main() {
   cout.tie(NULL);
 
 
-  int n, s;
   cin >> n >> s;
-
-  for(int i = 1; i <= n; i++) {
+  for(int i = 0; i < n; i++) {
     cin >> arr[i];
-    acuSum[i] = acuSum[i - 1] + arr[i];
   }
 
-  int l = 1, r = 1;
-  int minLength = INF, curSum = 0;
-  while(true) {
-    if (curSum < s) {
-      if (r > n) {
-        break;
-      }
+  int result = INF, i = 0, j = 0, sum = arr[0];
+  while(i <= j and j < n) {
+    if (sum >= s) {
+      result = min(result, j - i + 1);
 
-      curSum += arr[r];
-      r++;
+      sum -= arr[i];
+      i++;
     }
     else {
-      minLength = min(minLength, r - l);
-
-      curSum -= arr[l];
-      l++;
+      j++;
+      sum += arr[j];
     }
   }
 
-  cout << (minLength == INF ? 0 : minLength) << endl;
+  cout << (result == INF ? 0 : result) << endl;
 
   return 0;
 }
