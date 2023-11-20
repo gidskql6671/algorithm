4a9781cee78aa2bc6e1782280c62a3be7990e4df
DongHwan Kim <gidskql6671@gmail.com>
2023-11-20 14:35:09 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  int n;
+  cin >> n;
+
+  priority_queue<int, vector<int>, greater<int>> pq;
+  for(int i = 0; i < n; i++) {
+    int num;
+    cin >> num;
+
+    if (num == 0) {
+      if (pq.empty()) {
+        cout << "0\n";
+      }
+      else {
+        cout << pq.top() << "\n";
+        pq.pop();
+      }
+    }
+    else {
+      pq.push(num);
+    }
+  }
+
+  return 0;
+}
